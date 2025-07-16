70a4fe5de988b8196fc32536a2e0c831553d9e15
Utsav Balhara <utsavbalhara@gmail.com>
2025-07-16 19:01:46 +0530

    Initial Commit

---
+++ ProjectKisan/ProjectKisanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectKisanApp.swift
+//  ProjectKisan
+//
+//  Created by Utsav Balhara on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectKisanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
